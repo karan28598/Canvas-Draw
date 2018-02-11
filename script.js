1b53832b29186551f426ce016d0aedc1d0fbc13a
Karan Agrawal <mr.karanagrawal@gmail.com>
2018-02-12 00:56:56 +0530

    comments added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//Canvas variables
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var w = canvas.width;
 var h = canvas.height;
+
+//Store current and previous mouse position
 var mouse = {
     x: 0,
     y: 0
@@ -11,22 +14,29 @@ var last_mouse = {
     y: 0
 };
 
+//Image element
 var outputcanvas = document.getElementById('canvasout');
+
+//Store current value of pen color and width
 var pencol = document.getElementById('pen-color');
 var penwid = document.getElementById('pen-width');
 
+//Variable to check mouse hover
 var drawup = false;
+
+//Set pen values
 var drawcolor = 'black';
 var drawwidth = penwid.value;
 
-
-
+//Check for change in pen color and width
 pencol.addEventListener('change', fcolor);
 penwid.addEventListener('change', fwidth);
 
+//Execute fsave and fclear when respective buttons are clicked
 document.getElementById('xsave').addEventListener("click", fsave);
 document.getElementById('xclear').addEventListener("click", fclear);
 
+//Check for movement of mouse over the canvas
 canvas.addEventListener('mousemove', function (e) {
     last_mouse.x = mouse.x;
     last_mouse.y = mouse.y;
@@ -35,20 +45,22 @@ canvas.addEventListener('mousemove', function (e) {
     draw('move');
 }, false);
 
+//Check for click to draw
 canvas.addEventListener('mousedown', function (e) {
     draw('down');
 }, false);
 
+//Check for release of click to stop drawing
 canvas.addEventListener('mouseup', function (e) {
     draw('up');
 }, false);
 
+//Check for mouse outside the canvas
 canvas.addEventListener('mouseout', function (e) {
     draw('up');
 }, false);
 
-
-
+//Draw on Canvas if clicked
 function draw(a) {
     if (a == 'down') {
         drawup = true;
@@ -57,34 +69,45 @@ function draw(a) {
         drawup = false;
     }
     if (drawup) {
+    	//Start
         ctx.beginPath();
-        ctx.moveTo(last_mouse.x, last_mouse.y);
-        ctx.lineTo(mouse.x, mouse.y);
 
-        ctx.strokeStyle = drawcolor;
-        ctx.lineWidth = drawwidth;
+        ///From previous point to current point
+        ctx.moveTo(last_mouse.x, last_mouse.y); 
+        ctx.lineTo(mouse.x, mouse.y); 
 
+        //In selected color and width
+        ctx.strokeStyle = drawcolor; 
+        ctx.lineWidth = drawwidth; 
+
+        //Draw it
         ctx.stroke();
+
+        //Stop
         ctx.closePath();
     }
 
 
 }
 
+//Set color
 function fcolor() {
     drawcolor = pencol.value;
 }
 
+//Set width
 function fwidth() {
     drawwidth = penwid.value;
 }
 
+//Save
 function fsave() {
-    var dataURL = canvas.toDataURL();
-    outputcanvas.src = dataURL;
-    outputcanvas.style.display = 'inline';  
+    var dataURL = canvas.toDataURL(); //Take canvas data and change it to base64 image hex-code
+    outputcanvas.src = dataURL; //Set canvas image element's source to the image code from canvas
+    outputcanvas.style.display = 'inline'; //Display the image by converting the code back to an image
 }
 
+//Clear after confirmation
 function fclear() {
     var flag = confirm("Clear Everything?");
     if (flag) {
